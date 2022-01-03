ec232cbc9a0c5e4ba0b2870787d0c40314fb838f
Hal <hgrforrest@gmail.com>
2022-01-03 14:24:39 -0500

    additional buffering

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ console.log(buf)
 console.log(buf[0])
 console.log(buf[1])
 console.log(buf.toString())
+console.log(buf.length)
+
+buf.forEach(element => {
+	console.log(element)
+})
+
+buf.write('Good day!')
+
+console.log(buf.toString())
+
+const buf2 = Buffer.alloc(10)
+
+buf2.write('Good day!')
+buf2[0] = 111
+
+console.log(buf2.subarray(0).toString())
+
+const slice = buf2.subarray(0, 2)
+console.log(slice.toString())
+
+console.log(buf2.toString())
+
+const buf3 = Buffer.alloc(40)
+buf3.set(buf)
+console.log(buf3)
+
+const buf4 = Buffer.alloc(20)
+buf4.set(buf2.subarray(1,3), 1)
+console.log(buf4)
